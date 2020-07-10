0b5c2cb1d44983eab3ec6644f0480c802613e232
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-07-10 12:01:07 -0500

    project UPDATE done

--- data/routers/projectRouter.js
+++ data/routers/projectRouter.js
@@ -31,7 +31,20 @@ router.post('/', (req, res) => {
   })
 });
 
- //delete request done (need to add count to message)
+//update request
+router.put('/:id', (req, res) => {
+  const changes = req.body;
+   Projects.update(req.params.id, changes)
+   .then(project => {
+     res.status(200).json(project)
+   })
+   .catch( error => {
+     console.log(error)
+     res.status(500).json({ message: " There was an error updating the project" })
+   })
+ });
+
+ //delete request done
 router.delete('/:id', (req, res) => {
   Projects.remove(req.params.id)
   .then(project => {
