e96d798e2377222996826d5d4b8445a45c22989d
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-07-10 11:29:10 -0500

    DELETE request done

--- data/routers/actionRouter.js
+++ data/routers/actionRouter.js
@@ -8,9 +8,17 @@ const router = express.Router();
 const Actions = require('../helpers/actionModel');
 const Projects = require('../helpers/projectModel')
 
-
-router.post('/', (req, res) => {
-  //post request Create
+//come back to
+router.post('/id/actions', (req, res) => {
+ const newAction = req.body
+  Actions.insert({...newAction, project_id: req.project.id })
+  .then(action => {
+    res.status(201).json(action)
+  })
+  .catch(error => {
+    console.log(error)
+    res.status(500).json({ message: "Action could not be added" })
+  })
 })
 
 //Get request done
@@ -26,7 +34,7 @@ router.get('/', (req, res) => {
   })
 })
 
-//Get request done
+//Get request by ID done
 router.get('/:id', (req, res) => {
   Actions.get(req.params.id)
   .then(action => {
@@ -43,8 +51,16 @@ router.put('/', (req, res) => {
   //put request Update
 })
 
-router.delete('/', (req, res) => {
-  //delete request Destroy
+ //delete request Destroy
+router.delete('/:id', (req, res) => {
+  Actions.remove(req.params.id)
+  .then(action => {
+    res.status(204).json({ message: `${action} deleted` })
+  })
+  .catch(error => {
+    console.log(error)
+    res.status(500).json({ message: "There was an error deleting the action" })
+  })
 })
 
 
